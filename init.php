8dbb33386327837661ab4a3c433188e42bea4216
Андрей <andrey.buharov@gmail.com>
2026-05-31 08:21:09 +0300

    ДОбавлен пункт меню.

--- init.php
+++ init.php
@@ -7,3 +7,12 @@
 
 // Module version
 define('DBSETTING_VERSION', '1.0.5');
+
+Kohana::$config->load('menu')
+    ->set('dbsetting', array(
+        'title' => 'База данных',
+        'url' => 'dbsetting',
+        'icon' => 'fa-cog',
+        'order' => 110,
+       
+    ));
