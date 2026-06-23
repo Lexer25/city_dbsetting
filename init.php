f3d81fd0531b3cce15e5de531889259a476564d1
Андрей <andrey.buharov@gmail.com>
2026-06-23 10:01:30 +0300

    Изменил меню

--- init.php
+++ init.php
@@ -8,7 +8,7 @@
 // Module version
 define('DBSETTING_VERSION', '1.0.5');
 
-Kohana::$config->load('menu')
+Kohana::$config->load('adm')
     ->set('dbsetting', array(
         'title' => 'База данных',
         'url' => 'dbsetting',
